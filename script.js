2aa96dbc87b615591b8d28963b85793df31ef379
t-rogalski <tomaszrogalski04@gmail.com>
2025-02-25 13:07:51 +0100

    Dodanie funkcjonalności przycisków
    Malowanie, zmiana rozmiaru siatki, gumka

--- script.js
+++ script.js
@@ -2,23 +2,59 @@ const CONTAINER_SIZE = 768;
 
 const container = document.querySelector(".container");
 
-let gridSize = parseInt(prompt("How many columns and rows"));
-
+let gridSize = 16; //default value of gridSize
 let squareSize = CONTAINER_SIZE / gridSize;
+createGrid();
 
-for (let i = 0; i < gridSize; i++) {
-    const squaresColumn = document.createElement("div");
-    for (let j = 0; j < gridSize; j++) {
-        const square = document.createElement("div");
-        square.className = "square";
-        square.style.border = "1px solid black";
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
-        });
-        squaresColumn.appendChild(square);
+paint = document.querySelector(".color");
+paint.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        color(square, `black`);
+    });
+});
+
+sizeChanger = document.querySelector(".sizeChanger");
+sizeChanger.addEventListener("click", () => {
+    gridSize = parseInt(prompt("How many columns and rows"));
+    squareSize = CONTAINER_SIZE / gridSize;
+    createGrid();
+});
+
+erase = document.querySelector(".erase");
+erase.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        color(square, `white`);
+    });
+});
+
+function createGrid() {
+    clearGrid();
+    for (let i = 0; i < gridSize; i++) {
+        const squaresColumn = document.createElement("div");
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            square.style.border = "1px solid black";
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            color(square, `black`);
+            squaresColumn.appendChild(square);
+        }
+
+        container.appendChild(squaresColumn);
     }
+}
 
-    container.appendChild(squaresColumn);
+function color(element, color) {
+    element.addEventListener("mouseover", () => {
+        element.style.backgroundColor = `${color}`;
+    });
+}
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
